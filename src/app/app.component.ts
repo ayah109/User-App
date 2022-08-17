99dcedb4f6c855377433ccf3100478ba00bc5dc6
Aya Hussain <ayatalep816@gmail.com>
2022-08-17 23:02:29 +0300

    basic user form1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
-import { NgForOf } from '@angular/common';
+// import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+// import { NgForm } from '@angular/forms';
 
-interface UserVM{
-  name: string,
-  numb: number,
-   age: number, 
-   email:string,
-   pass:number;
-}
 
 @Component({
   selector: 'app-root',
@@ -17,34 +10,6 @@ interface UserVM{
 })
 
 export class AppComponent {
-  UserList: UserVM[]=[
-    {
-      name:'Aya ',
-      numb: 597902289,
-      age: 22,
-      email:'aya@kljlklfdkl.com',
-      pass:12345
-  }
-   ];
   
-public newUser: UserVM ={name:'', numb:0, age: 0, email:'', pass:0 }
-
-
-    addUser(form: NgForm){
-      console.log(form.form.value,this.newUser);
-      if(!form.form.valid){
-        form.form.markAllAsTouched();
-      }else{
-        this.UserList.push(this.newUser);
-      }
-    }
-    
-  deleteUser(user:UserVM){
-    let i = this.UserList.indexOf(user);
-    if (i>= 0){
-      this.UserList.splice(i,1);
-    }
-  }
-
 }
   
